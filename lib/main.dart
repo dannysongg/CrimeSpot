cfdfdb18c149bf6b7ba1615f29fdf8541913737c
dannysongg <dannysongg@gmail.com>
2021-02-16 23:52:33 -0800

    Added settings drawer (Non-functional)

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,50 @@ class MyApp extends StatelessWidget{
               SearchBar(),
             ]
         ),
+        drawer: AppDrawer(),
         bottomNavigationBar: BottomNavBar(),
+      ),
+    );
+  }
+}
+
+class AppDrawer extends StatefulWidget {
+  @override
+  _AppDrawerState createState() => _AppDrawerState();
+}
+
+class _AppDrawerState extends State<AppDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('CrimeSpot'),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title:Text('Map Settings'),
+            onTap: () {
+
+            },
+          ),
+          ListTile(
+            title:Text('User Settings'),
+            onTap: () {
+
+            },
+          ),
+          ListTile(
+            title:Text('Misc Settings'),
+            onTap: () {
+
+            },
+          ),
+        ],
       ),
     );
   }
